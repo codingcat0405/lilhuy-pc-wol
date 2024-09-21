e8511fa1b28e526b4139fbd7309a6a748979ac6e
lilhuy <luuhuyulei@gmail.com>
2024-09-21 22:53:42 +0700

    init project

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
 import { Elysia } from "elysia";
+import { cors } from '@elysiajs/cors'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(cors())
+  .get("/", () => "Hello Elysia")
+  .get("/wol/:mac",  async ({params}) => {
+    const mac = params.mac;
+    const proc = Bun.spawn(['wakeonlan', mac]);
+    const output = await new Response(proc.stdout).text();
+    return {
+      status: 'success',
+      message: 'WOL packet sent'
+    }
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
